3f927cd0b120248e6cd3ad2d0e7084c2b4dc7c3b
alexmcoelho <alexma_coelho@hotmail.com>
2020-06-15 22:43:00 -0300

    Projects CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
+const { celebrate, Joi } = require('celebrate');
 
 const app = express();
 
@@ -10,24 +10,83 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (_, response) => {
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post(
+  "/repositories", 
+  celebrate({
+      body: Joi.object().keys({
+          title: Joi.string().required(),
+          url: Joi.string().required().regex(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/),
+          techs: Joi.array().items(Joi.string().required())
+      })
+  }, {
+      abortEarly: false
+  }),
+  (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put(
+  "/repositories/:id", 
+  celebrate({
+      body: Joi.object().keys({
+          title: Joi.string(),
+          url: Joi.string().regex(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/),
+          techs: Joi.array().items(Joi.string()),
+          likes: Joi.number()
+      })
+  }, {
+      abortEarly: false
+  }),
+  (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  let repository = repositories.find(repository => repository.id === id);
+  if(!repository) {
+    return response.status(400).send();
+  }
+  repository = { ...repository, title, url, techs };
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repository => repository.id === id);
+  if(index < 0) {
+    return response.status(400).send();
+  }
+  repositories.splice(index, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+  if(!repository) {
+    return response.status(400).send();
+  }
+
+  repository.likes++;
+
+  return response.json(repository);
 });
 
 module.exports = app;
